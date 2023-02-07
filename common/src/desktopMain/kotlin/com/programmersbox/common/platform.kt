4ab533e3a7c2a5d89a0a0ed8c3c779efc8c16f60
Jacob Rein <jakerein@gmail.com>
2023-02-07 12:21:03 -0500

    Cleaning up desktop sharing

--- common/src/desktopMain/kotlin/com/programmersbox/common/platform.kt
+++ common/src/desktopMain/kotlin/com/programmersbox/common/platform.kt
@@ -1,22 +1,12 @@
 package com.programmersbox.common
 
 import androidx.compose.runtime.Composable
-import java.awt.Toolkit
-import java.awt.datatransfer.Clipboard
-import java.awt.datatransfer.StringSelection
 
 public actual fun getPlatformName(): String {
     return "Desktop"
 }
 
 @Composable
-public fun UIShow() {
-    App(
-        onShareClick = {
-            val stringSelection = StringSelection(it.quote)
-            val clipboard: Clipboard = Toolkit.getDefaultToolkit().systemClipboard
-            clipboard.setContents(stringSelection, null)
-            Toolkit.getDefaultToolkit().beep()
-        }
-    )
+public fun UIShow(onShareClick: (SavedQuote) -> Unit) {
+    App(onShareClick = onShareClick)
 }
--- desktop/src/jvmMain/kotlin/Main.kt
+++ desktop/src/jvmMain/kotlin/Main.kt
@@ -1,15 +1,37 @@
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FormatQuote
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.window.*
 import com.programmersbox.common.UIShow
+import java.awt.Toolkit
+import java.awt.datatransfer.Clipboard
+import java.awt.datatransfer.StringSelection
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Quotes",
+        icon = rememberVectorPainter(Icons.Default.FormatQuote)
+    ) {
         MaterialTheme(colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()) {
-            UIShow()
+            val trayState = rememberTrayState()
+            Tray(
+                state = trayState,
+                icon = rememberVectorPainter(Icons.Default.FormatQuote),
+            )
+            UIShow(
+                onShareClick = {
+                    val stringSelection = StringSelection(it.quote)
+                    val clipboard: Clipboard = Toolkit.getDefaultToolkit().systemClipboard
+                    clipboard.setContents(stringSelection, null)
+                    Toolkit.getDefaultToolkit().beep()
+                    trayState.sendNotification(Notification("Copied!", it.quote))
+                }
+            )
         }
     }
 }
